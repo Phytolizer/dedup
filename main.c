98ac8579cd6ec4b345b088507946ea9179ec2852
Kyle Coffey <me@phytolizer.dev>
2022-09-06 11:12:12 -0500

    load a hash table

--- main.c
+++ main.c
@@ -1,12 +1,26 @@
 #include "recdir.h"
+#include "stb_ds.h"
 #include "str.h"
 
 #include <dirent.h>
 #include <errno.h>
 #include <sha256.h>
+#include <sodium/randombytes.h>
+#include <stdint.h>
 #include <stdio.h>
 
 #define UNUSED(x) (void)(x)
+
+typedef struct
+{
+    uint8_t value[SHA256_BLOCK_SIZE];
+} Hash;
+
+typedef struct
+{
+    Hash key;
+    str* value;
+} Record;
 
 static char hex_digit(uint8_t digit)
 {
@@ -18,17 +32,17 @@ static char hex_digit(uint8_t digit)
     return (char)('a' + (digit - 10));
 }
 
-static void hash_as_cstr(BYTE hash[static SHA256_BLOCK_SIZE], char out[static SHA256_BLOCK_SIZE * 2 + 1])
+static void hash_as_cstr(Hash hash, char out[static SHA256_BLOCK_SIZE * 2 + 1])
 {
     for (size_t i = 0; i < SHA256_BLOCK_SIZE; i++)
     {
-        out[i * 2] = hex_digit(hash[i] >> 4);
-        out[i * 2 + 1] = hex_digit(hash[i] & 0x0F);
+        out[i * 2] = hex_digit(hash.value[i] >> 4);
+        out[i * 2 + 1] = hex_digit(hash.value[i] & 0x0F);
     }
     out[(size_t)SHA256_BLOCK_SIZE * 2] = '\0';
 }
 
-static void hash_of_file(str file_path, BYTE out[static SHA256_BLOCK_SIZE])
+static void hash_of_file(str file_path, Hash* out)
 {
     FILE* fp = fopen(file_path.ptr, "rb");
     if (fp == NULL)
@@ -56,10 +70,10 @@ static void hash_of_file(str file_path, BYTE out[static SHA256_BLOCK_SIZE])
 
         sha256_update(&ctx, buf, nread);
     }
-    sha256_final(&ctx, out);
+    sha256_final(&ctx, out->value);
 }
 
-static void print_files_recursively(str dir_path)
+static void print_files_recursively(str dir_path, Record** db)
 {
     RECDIR* recdir = recdir_open(dir_path);
     if (recdir == NULL)
@@ -80,12 +94,21 @@ static void print_files_recursively(str dir_path)
             break;
         }
         str path = str_fmt(STR_FMT "/%s", STR_ARG(recdir_path(recdir)), ent->d_name);
-        BYTE hash[SHA256_BLOCK_SIZE];
-        hash_of_file(path, hash);
+        Hash hash;
+        hash_of_file(path, &hash);
         char cstr[SHA256_BLOCK_SIZE * 2 + 1];
         hash_as_cstr(hash, cstr);
-        printf(STR_FMT " => %.*s\n", STR_ARG(path), (int)sizeof(cstr) - 1, cstr);
-        str_free(path);
+        ptrdiff_t i = stbds_hmgeti(*db, hash);
+        if (i < 0)
+        {
+            Record record = {.key = hash, .value = NULL};
+            stbds_arrput(record.value, path);
+            stbds_hmputs(*db, record);
+        }
+        else
+        {
+            stbds_arrput((*db)[i].value, path);
+        }
     }
     recdir_close(recdir);
 }
@@ -95,5 +118,21 @@ int main(int argc, char** argv)
     UNUSED(argc);
     UNUSED(argv);
 
-    print_files_recursively(str_lit("."));
+    size_t seed = 0;
+    randombytes_buf(&seed, sizeof(seed));
+    stbds_rand_seed(seed);
+
+    Record* db = NULL;
+
+    print_files_recursively(str_lit("."), &db);
+
+    for (long i = 0; i < stbds_hmlen(db); i++)
+    {
+        for (long j = 0; j < stbds_arrlen(db[i].value); j++)
+        {
+            str_free(db[i].value[j]);
+        }
+        stbds_arrfree(db[i].value);
+    }
+    stbds_hmfree(db);
 }
--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,6 @@
 set_defaultmode "dbg"
 includes "crypto-algorithms"
+add_requires "libsodium"
 
 target "dedup"
     set_kind "binary"
@@ -10,13 +11,14 @@ target "dedup"
         "-Wpedantic",
         "-Wmissing-prototypes",
         "-Wno-unused-function",
-        "-std=c11",
-        "-D_DEFAULT_SOURCE",
+        "-std=gnu11",
         "-ggdb",
         "-fsanitize=address",
         "-fno-omit-frame-pointer",
     }
+    add_defines "STBDS_NO_SHORT_NAMES"
     add_ldflags { "-fsanitize=address" }
     add_links { "sha256" }
+    add_packages { "libsodium" }
     add_linkdirs "$(buildir)/lib"
     add_includedirs "crypto-algorithms/repo"
